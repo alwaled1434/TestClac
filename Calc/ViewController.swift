4c61c3b149fbf5722b414344b8d9804f3bd27640
Designer <alwaled1434@gmail.com>
2019-06-16 02:52:46 +0300

    initial commit

---
+++ Calc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calc
+//
+//  Created by Desginer on 09/08/1440 AH.
+//  Copyright © 1440 Desginer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
